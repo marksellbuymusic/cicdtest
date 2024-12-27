5ee21ab82c48a1461be756dc104acf3f7180a64f
mark <mark.an@juice.co.kr>
2024-12-27 13:34:38 +0900

    (fix) fix small

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           priority
         />
       </div>
-      <h1>HI THIS IS FIRST PAGE</h1>
+      <h1>HI THIS IS SECOND PAGE</h1>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
   );
